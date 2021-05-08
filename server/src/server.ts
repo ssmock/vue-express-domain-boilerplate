4980af4e950a6d0d3ccc99d7c7639cdfe9a2b313
ssmock <ssmock@MSI.localdomain>
2021-05-08 15:11:41 -0400

    Rename express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,22 +3,22 @@ import cors from "cors";
 import { Thing, Things } from "../../domain/Thing";
 
 const port = 5000;
-const server = express();
+const app = express();
 
 // TODO: Set this value dynamically
-server.use(cors({
+app.use(cors({
   origin: [
     "http://localhost:3000", 
     "http://172.22.53.52:8080" /* WSL quirk -- sometimes localhost doesn't work */ ]
 }))
 
-server.get("/thing/:id", async (req, resp) => {
+app.get("/thing/:id", async (req, resp) => {
   const d: Thing = await Things.make(`GET thing from the server ${req.params.id}`, new Date().toISOString());
 
   return resp.json(d);
 });
 
-server.get("/thing/:id/slow", async (req, resp) => {
+app.get("/thing/:id/slow", async (req, resp) => {
   const d: Thing = await Things.make(`GET slow thing from the server ${req.params.id}`, new Date().toISOString());
 
   setTimeout(() => {
@@ -26,12 +26,12 @@ server.get("/thing/:id/slow", async (req, resp) => {
   }, 2000);
 });
 
-server.post("/thing/:id", async (req, resp) => {
+app.post("/thing/:id", async (req, resp) => {
   const d: Thing = await Things.make(`POST thing from the server ${req.params.id}`, new Date().toISOString());
 
   return resp.json(d);
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
 });
